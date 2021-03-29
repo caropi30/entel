4e0cc969133198991d7bd0bf27b55444cdfc45f1
Carolina Pirela <caropi30@gmail.com>
2021-03-29 19:31:01 -0300

    primer commit

---
+++ js/cart.js
@@ -0,0 +1,27 @@
+class Equipment {
+    constructor(shoppingCart) {
+        let cart = shoppingCart
+
+        this.cartCalulation = () => {
+            let total = 0
+            for (let equipment of cart) {
+                total += parseFloat(equipment.price)
+            }
+
+            return total
+        }
+
+        this.discountApply = () => {
+            let amountWithDiscount = 0
+            let amountWithoutDscount = this.cartCalulation()
+            let r = DISCOUNT.find(d => d.amount == parseInt(cart.length))
+            if (r == undefined) {
+                console.log("No hay descuentos para tu equipo seleccionado")
+            } else {
+                amountWithDiscount = (amountWithoutDscount * parseFloat(r.factor))
+            }
+
+            return amountWithDiscount
+        }
+    } 
+}
---
+++ js/logic.js
@@ -0,0 +1,36 @@
+let equipment = "";
+let cart = [];
+const btnAddDevice = document.getElementById("btnAddDevice");
+const catalogue = document.getElementById("catalogue");
+
+let loadEquipmentCard = () => {
+    debugger
+    catalogue.innerHTML = " "
+    for (let equipment of CATALOGUEEQUIPMENT) {
+        let card = `<div id="equipmentCard" class="card shadow-sm">
+                        <button id="equipmentFeatures" class="btn" type="button" data-toggle="modal" data-target="#featuresModal">
+                            <img src="${equipment.image}">
+                        </button>
+                        <div class="card-body">
+                            <h5 class="card-title">${equipment.name}</h5>
+                            <p class="card-text font-weight-lighter">${equipment.brand}</p>
+                            <h6 class="card-title">$ ${equipment.price}</h6>
+                        </div>
+                        <button id="btnAddDevice" type="button" class="btn btn-lg btn-pink px-4 card-footer" onclick="addDeviceToCart(${equipment.equipmentid})" value="Agregar">Agregar</button>
+                    </div>`
+
+                    catalogue.innerHTML += card
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadEquipmentCard) 
+
+/*let addDeviceToCart = (id) => {
+    let r = CATALOGUEEQUIPMENT.find(c => c.equipmentid == id)
+    cart.push(r)
+
+    cartRefresh();
+    itemsCartSave();
+}
+*/
+
