900b8c09bcbf3eac860a306e7ee2b2d94792d167
Carolina Pirela <caropi30@gmail.com>
2021-04-03 17:03:08 -0300

    agregando jquery

--- js/logic.js
+++ js/logic.js
@@ -7,7 +7,7 @@ let loadEquipmentCard = () => {
     debugger
     catalogue.innerHTML = " "
     for (let equipment of CATALOGUEEQUIPMENT) {
-        let card = `<div id="equipmentCard" class="card shadow-sm">
+     /*   let card = `<div id="equipmentCard" class="card shadow-sm">
                         <button id="equipmentFeatures" class="btn" type="button" data-toggle="modal" data-target="#featuresModal">
                             <img src="${equipment.image}">
                         </button>
@@ -19,11 +19,27 @@ let loadEquipmentCard = () => {
                         <button id="btnAddDevice" type="button" class="btn btn-lg btn-pink px-4 card-footer" onclick="addDeviceToCart(${equipment.equipmentid})" value="Agregar">Agregar</button>
                     </div>`
 
-                    catalogue.innerHTML += card
+                    catalogue.innerHTML += card*/
+        debugger
+        $(this.#equipmentCard).append(`<div class="card shadow-sm">
+                                        <button id="equipmentFeatures" class="btn" type="button" data-toggle="modal" data-target="#featuresModal">
+                                            <img src="${equipment.image}">
+                                        </button>
+                                        <div class="card-body">
+                                            <h5 class="card-title">${equipment.name}</h5>
+                                            <p class="card-text font-weight-lighter">${equipment.brand}</p>
+                                            <h6 class="card-title">$ ${equipment.price}</h6>
+                                        </div>
+                                        <button id="btnAddDevice" type="button" class="btn btn-lg btn-pink px-4 card-footer" onclick="addDeviceToCart(${equipment.equipmentid})" value="Agregar">Agregar</button>
+                                    </div>`)
+
+                    $('#catalogue').html('')
     }
 }
 
-document.addEventListener("DOMContentLoaded", loadEquipmentCard) 
+//document.addEventListener("DOMContentLoaded", loadEquipmentCard) 
+
+$(document).on("DOMContentLoaded", loadEquipmentCard)
 
 /*let addDeviceToCart = (id) => {
     let r = CATALOGUEEQUIPMENT.find(c => c.equipmentid == id)
